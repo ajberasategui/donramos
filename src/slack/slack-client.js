41c6d3a69e6e4606429ea81df232ac8d7877dc1b
Agustin <Agustin@MacBook-Pro.local>
2016-06-01 10:42:17 -0300

    promisify removed from getUsers

--- src/slack/slack-client.js
+++ src/slack/slack-client.js
@@ -1,9 +1,8 @@
 var Slack = require('slack-node');
 var slack = new Slack(process.env.token);
-var promisify = require('promisify-node');
 
 module.exports = {
-    getUsers: promisify(getUsers) 
+    getUsers: getUsers 
 };
 
 function getUsers(callback) {
