35f309d4ee9afc3f7bab0889095dd37dd23e8f45
Agustin Berasategui <ajbersategui@gmail.com>
2016-06-05 20:38:44 -0300

    Firebase config added

--- src/config.js
+++ src/config.js
@@ -2,9 +2,12 @@ module.exports = {
     DATE_FORMAT: 'DD/MM/YYYY HH:mm',
     STORAGE_FILE: './storage/db',
     HEARS_DIR: 'src/hears/',
+    HEARS_DIR_REL_TO_CORE: '../hears/',
     HEAR_ENVS: {
         'ALL': 'direct_message,direct_mention,mention,ambient',
         'MENTION_AND_DIRECT': 'direct_message,direct_mention,mention'
     },
-    myName: "/don(.*)ramos/i"
+    myName: "/don(.*)ramos/i",
+    FBCREDS: "./firebaseCreds.json",
+    FBDBURL: "https://don-ramos.firebaseio.com"
 };
